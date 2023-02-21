0d832e118bf3caab4fe6db7f42fc0d5609b45a15
PierrickHOUEE <houee.pierrick@gmail.com>
2023-02-21 21:08:38 +0100

    Weird bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+// FS can only be used on the server side, not on the client side
+import fs from "fs/promises";
+// Module helping to build path
+import path from "path";
 
 function HomePage(props: any) {
   const { products } = props;
@@ -17,9 +21,18 @@ function HomePage(props: any) {
 // Runs invisible code to the user (doesn't run on client side)
 // Is executed on build
 export async function getStaticProps() {
+  // Is asynchronous because we imported fs/promises
+  // cwd = Current Working Directory
+  // process.cwd() givew the cwd of the current file
+  // In nextJS the CWD will be the overall project directory
+  // Add arguments to build the string
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const JSONdata = await fs.readFile(filePath);
+  const data = JSON.parse(JSONdata.toString());
+
   return {
     props: {
-      products: [{ id: "p1", title: "Product 1" }],
+      products: data?.products,
     },
   };
 }
