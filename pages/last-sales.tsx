22757aea3002b05bbb345b7d1b67a56b4d21f8ac
Pierrick Houée <houee.pierrick@gmail.com>
2023-03-03 22:14:14 +0100

    WIP

--- pages/last-sales.tsx
+++ pages/last-sales.tsx
@@ -1,15 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+interface Sales {
+  id: any;
+  username: any;
+  volume: any;
+}
 
 function LastSalesPage() {
+  const [sales, setSales] = useState<Sales[]>();
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
+    setIsLoading(true);
     // Fetch is an API available in the navigator
     // Using Firebase as a mock database (adding /sales.json to url)
     fetch(
       "https://nextjs-course-1541c-default-rtdb.europe-west1.firebasedatabase.app/sales.json"
-    ).then();
+      // Taking the response as a json (is a promise, so requires an addtional then)
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const transformedSales: any[] = [];
+        for (const key in data) {
+          transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+          });
+        }
+        setSales(transformedSales);
+        setIsLoading(false);
+      });
   }, []);
 
-  return <ul></ul>;
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (!sales) {
+    return <p>No data yet</p>;
+  }
+  return (
+    <ul>
+      {sales?.map((sale: Sales, index: number) => {
+        return (
+          <li key={sale.id}>
+            {sale.username} - ${sale.volume}
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default LastSalesPage;
